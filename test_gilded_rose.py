a9522151d0804891a85ccbae648ca48258da91ff
Emma Gale <emmajgale@hotmail.com>
2024-10-05 10:46:35 +0100

    add test for dexterity vest

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -2,15 +2,18 @@
 import unittest
 
 from gilded_rose import Item, GildedRose
-
+Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+
+
+    def test_dexterity_vest(self):
+        items = [Item(name="+5 Dexterity Vest", sell_in=10, quality=20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("+5 Dexterity Vest", items[0].name)
 
         
 if __name__ == '__main__':
     unittest.main()
+
